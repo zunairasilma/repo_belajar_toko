9d093d08d48d58c631f2588310bea70d8ca4d6f8
zunairasilma <zunaira_silma_28rpl@student.smktelkom-mlg.sch.id>
2020-09-04 20:11:20 +0700

    Menambah fungsi READ

--- app/Http/Controllers/CustomersController.php
+++ app/Http/Controllers/CustomersController.php
@@ -8,6 +8,10 @@ use Illuminate\Support\Facades\Validator;
 
 class CustomersController extends Controller
 {
+    public function show()
+    {
+        return customers::all();
+    }
     public function store(Request $request)
     {
     	$validator=Validator::make($request->all(),
